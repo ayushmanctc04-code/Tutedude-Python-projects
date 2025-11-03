15b0f42a8bf723e0ccccc895f846fed7f9ccba52
Ayushman Pati <ayushmanctc04@gmail.com>
2025-11-03 14:37:14 +0530

    Add files via upload

---
+++ Basic Calculation Task.py
@@ -0,0 +1,12 @@
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
